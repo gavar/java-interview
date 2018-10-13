f6bed907632340ac3a5d83bdb8edf47c95419178
Max Stankevich <max.stankevich.me@gmail.com>
2018-10-13 20:46:35 +0300

    ctci: question 1.1

---
+++ src/main/java/com/ctci/chapter1/Q1.java
@@ -0,0 +1,110 @@
+package com.ctci.chapter1;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.Arrays;
+import java.util.HashSet;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+/**
+ * Is Unique:
+ * Implement an algorithm to determine if a string has all unique characters.
+ * What if you cannot use additional data structures?
+ */
+class Q1 {
+
+    /**
+     * Runtime: O(N * logN)
+     * Space: O(N)
+     */
+    static boolean isUniqueByHashMap(String value) {
+        HashSet<Character> chars = new HashSet<>();
+        for (int i = 0; i < value.length(); i++) // N
+            if (!chars.add(value.charAt(i))) // logN
+                return false;
+
+        return true;
+    }
+
+    /**
+     * Runtime: O(N)
+     * Space: O(k)
+     * k - size of alphabet
+     */
+    static boolean isUniqueByArray(String value) {
+        boolean chars[] = new boolean[128];
+        for (int i = 0; i < value.length(); i++) { // N
+            char c = value.charAt(i);
+            if (chars[c]) return false;
+            else chars[c] = true;
+        }
+        return true;
+    }
+
+    /**
+     * Runtime: O(N)
+     * Space: O(1)
+     */
+    static boolean isUniqueByBitShift(String value) {
+        int checker = 0;
+        for (int i = 0; i < value.length(); i++) { // N
+            int mask = 1 << (value.charAt(i) - 'a');
+            if ((checker & mask) != 0) return false;
+            checker |= mask;
+        }
+        return true;
+    }
+
+    /**
+     * What if you cannot use additional data structures?
+     * Runtime: O(N * logN)
+     * Space: O(1)
+     */
+    static boolean isUniqueBySort(char[] value) {
+        Arrays.sort(value); // N * log N
+        for (int i = 1; i < value.length; i++) // N
+            if (value[i] == value[i - 1])
+                return false;
+
+        return true;
+    }
+
+    /**
+     * What if you cannot use additional data structures?
+     * Runtime: O(N^2) | N * (N - 1) / 2
+     * Space: O(1)
+     */
+    static boolean isUniqueByPairs(String value) {
+        for (int i = 0; i < value.length(); i++) // N
+            for (int j = i + 1; j < value.length(); j++) // N - 1
+                if (value.charAt(i) == value.charAt(j))
+                    return false;
+        return true;
+    }
+
+
+    @ParameterizedTest
+    @ValueSource(strings = {"abcde", "kite", "padle"})
+    void checkUnique(String value) {
+        String msg = String.format("'%s' have all unique characters", value);
+        assertTrue(Q1.isUniqueByHashMap(value), msg);
+        assertTrue(Q1.isUniqueByArray(value), msg);
+        assertTrue(Q1.isUniqueBySort(value.toCharArray()), msg);
+        assertTrue(Q1.isUniqueByPairs(value), msg);
+        assertTrue(Q1.isUniqueByBitShift(value), msg);
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"hello", "apple", "abcddd", "aaabcd"})
+    void checkNonUnique(String value) {
+        String msg = String.format("'%s' doesn't have all unique characters", value);
+        assertFalse(Q1.isUniqueByHashMap(value), msg);
+        assertFalse(Q1.isUniqueByArray(value), msg);
+        assertFalse(Q1.isUniqueBySort(value.toCharArray()), msg);
+        assertFalse(Q1.isUniqueByPairs(value), msg);
+        assertFalse(Q1.isUniqueByBitShift(value), msg);
+    }
+}
---
+++ src/main/java/com/ctci/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * Provides solutions of question from the book: "Cracking the Coding Interview, 6th Edition"
+ * http://www.crackingthecodinginterview.com
+ */
+package com.ctci;
